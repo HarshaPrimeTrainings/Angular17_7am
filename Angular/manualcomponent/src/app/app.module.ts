9ff15c4a9eb0c77a80e3ea7b0ec96ab56d3b592a
HarshaVardhan <athaluri.harsha@gmail.com>
2024-06-04 07:32:48 +0530

    manual component

---
+++ Angular/manualcomponent/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DemoComponent } from './democomponent';
+import { MyComponent } from './mycomponent';
+import { HeaderComponent } from './header/header.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,DemoComponent,MyComponent, HeaderComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
